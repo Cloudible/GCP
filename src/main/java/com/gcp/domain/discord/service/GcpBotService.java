403b7f22ca66945a15b591b2f9ef8376dd5c9945
Jinyoung <glitt5384@naver.com>
2025-08-03 08:43:14 +0900

    fix: http -> https

--- src/main/java/com/gcp/domain/discord/service/GcpBotService.java
+++ src/main/java/com/gcp/domain/discord/service/GcpBotService.java
@@ -54,7 +54,7 @@ public class GcpBotService extends ListenerAdapter {
                         .encodeToString(infoRaw.getBytes(StandardCharsets.UTF_8));
 
                 String redirectUri = UriComponentsBuilder
-                        .fromHttpUrl("https://gcpassist.com:8080/oauth2/authorization/google")
+                        .fromHttpUrl("https://gcpassist.com:8443/oauth2/authorization/google")
                         .queryParam("access_type", "offline")
                         .queryParam("mode", "login")
                         .queryParam("redirect_uri", "https://gcpassist.com?info=" + encodedInfo)
