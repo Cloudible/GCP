49f08e65c707133281abaede07fc466b0f52d207
Jinyoung <glitt5384@naver.com>
2025-08-20 18:15:17 +0900

    feat: @Transactional 추가

--- src/main/java/com/gcp/domain/gcp/service/GcpProjectCommandServiceImpl.java
+++ src/main/java/com/gcp/domain/gcp/service/GcpProjectCommandServiceImpl.java
@@ -6,9 +6,11 @@ import com.gcp.domain.gcp.entity.GcpProject;
 import com.gcp.domain.gcp.repository.GcpProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class GcpProjectCommandServiceImpl implements GcpProjectCommandService{
 
     private final GcpProjectRepository gcpProjectRepository;
